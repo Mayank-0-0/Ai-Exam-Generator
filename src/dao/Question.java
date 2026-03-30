495b32663363ccbd7fc37b5632971225b66d7477
Mayank-0-0 <bishtmayank205@gmail.com>
2026-03-30 19:03:35 +0530

    question Model

---
+++ src/dao/Question.java
@@ -0,0 +1,73 @@
+package dao;
+
+public class Question {
+    private String question_Text;
+    private int questionID;
+    private int subjectID;
+    private String optionA,optionB,optionC,optionD;
+    private String answer;
+
+    public String getQuestion_Text() {
+        return question_Text;
+    }
+
+    public void setQuestion_Text(String question_Text) {
+        this.question_Text = question_Text;
+    }
+
+    public int getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
+
+    public int getSubjectID() {
+        return subjectID;
+    }
+
+    public void setSubjectID(int subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
